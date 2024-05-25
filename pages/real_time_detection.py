965f33010336c41ce684bca09ac65a66cc5a36e5
benmo1221 <114561467+benmo1221@users.noreply.github.com>
2024-05-25 21:38:12 +0300

    Update real_time_detection.py

--- pages/real_time_detection.py
+++ pages/real_time_detection.py
@@ -5,9 +5,19 @@ import numpy as np
 import streamlit as st
 from PIL import Image
 import av
-from inference import predict
 from streamlit_webrtc import webrtc_streamer
 
+def predict(model, frame):
+    """Generate predictions and annotate the predicted frame."""
+    resx = model(frame, size = 416).crop(save=False)
+    for d in resx:
+        box = list(map(int, list(map(np.round, d['box']))))
+        label = d['label']
+        cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 3)
+        px, py = box[0], box[1]-10
+        cv2.putText(frame, label, (px, py), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    return frame
+    
 @st.cache(allow_output_mutation=True)
 def load_model():
     """Load model from hub with custom weights."""
